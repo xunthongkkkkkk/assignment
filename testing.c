c59e71bb0a59279282a92693e3a07c695f11f05d
Ryan Sim <59084058+RyanSimYH@users.noreply.github.com>
2021-11-02 10:41:32 +0800

    Added code to calculate NB Probability

--- testing.c
+++ testing.c
@@ -30,6 +30,12 @@ struct feature
     float sit;
 };
 
+// Function to calculate the prior and posterior probability for NB
+void NBProbability();
+
+// 
+void NBcount(int outcome, int i);
+
 //So that it can read 100 line of text file
 struct dataset volunteers[100];
 
@@ -113,5 +119,147 @@ int main(void) {
                 testingF[i-80].smoke, testingF[i-80].sit, testingO[i-80]);
             */
         }
+    }
+}
+
+void NBProbability()
+{
+    //Loops through the total size of the training set. Gets the limit from the size of the output array as output array and features strut are One to One
+    for(int i = 0; i < sizeof(trainingO); i++)
+    {
+        //Determines if the sample is normal(0) or altered(1)
+        switch (trainingO[i])
+        {
+        case 0 : //normal
+            NBcount(0,i);
+            break;
+        
+        case 1: //altered
+            NBcount(1,i);
+            break;
+        }
+    }
+    //Determining posterior probability
+    for (int k = 0; k < 2; k++)
+    {
+        for (int j = 1; j < 22; j++)
+        {
+            Probability[k][j] = Probability[k][j]/Probability[k][0];
+            printf("\n outcome: %d Variable: %d Probability: %f", k, j, Probability[k][j]);
+        }
+        
+    }
+
+    //determining prior probability
+    Probability[0][0] = Probability[0][0]/sizeof(trainingO);
+    Probability[1][0] = Probability[1][0]/sizeof(trainingO);
+    printf("\nPrior Prob:\n Normal: %f \n Altered: %f", Probability[0][0],Probability[1][0]);
+    
+}
+
+void NBcount(int outcome, int i)
+{
+    // Count for the different seasons
+    Probability[outcome][0]++;
+    if (trainingF[i].season == -1) //Winter
+    {
+        Probability[outcome][1]++;
+    }
+    else if (trainingF[i].season == -0.33) //Spring
+    {
+        Probability[outcome][2]++;
+    }
+    else if (trainingF[i].season == 0.33) //Summer
+    {
+        Probability[outcome][3]++;
+    }
+    else if (trainingF[i].season == 1) //Fall
+    {
+        Probability[outcome][4]++;
+    }
+    
+    //Determining the count for the disease
+    switch (trainingF[i].disease)
+    {
+    case 0: //Yes
+        Probability[outcome][5]++;
+        break;
+    
+    case 1: //No
+        Probability[outcome][6]++;
+        break;
+    }
+
+    switch (trainingF[i].trauma)
+    {
+    case 0: //Yes
+        Probability[outcome][7]++;
+        break;
+    
+    case 1: //No
+        Probability[outcome][8]++;
+        break;
+    }
+
+    switch (trainingF[i].surgical)
+    {
+    case 0: //Yes
+        Probability[outcome][9]++;
+        break;
+    
+    case 1: //No
+        Probability[outcome][10]++;
+        break;
+    }
+
+    switch (trainingF[i].fever)
+    {
+    case -1: //< 3 months ago
+        Probability[outcome][11]++;
+        break;
+    
+    case 0: // > 3 months ago
+        Probability[outcome][12]++;
+        break;
+
+    case 1: // no fever
+        Probability[outcome][13]++;
+        break;     
+    }
+
+    if (trainingF[i].freq == 0.2) // Several times a day
+    {
+        Probability[outcome][14]++;
+    }
+    else if (trainingF[i].freq == 0.4) // Every Day
+    {
+        Probability[outcome][15]++;
+    }
+    else if (trainingF[i].freq == 0.6) // Several Times a week
+    {
+        Probability[outcome][16]++;
+    }
+    else if (trainingF[i].freq == 0.8) // Once a week
+    {
+        Probability[outcome][17]++;
+    }
+    else if (trainingF[i].freq == 1) // Hardly
+    {
+        Probability[outcome][18]++;
+    }
+    
+    switch (trainingF[i].smoke)
+    {
+    case -1:  //never
+        Probability[outcome][19]++;
+        break;
+    
+    case 0: // occasional
+        Probability[outcome][20]++;
+        break;
+
+    case 1: // daily
+        Probability[outcome][21]++;
+        break;     
     }
 }
